19568fab46901dc22bd994983977c26d78532307
jamezzz123 <chinwokwuj@gmail.com>
2023-02-08 17:03:32 +0100

    feat: initalize Quasar app

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {},
+
+  setup() {
+    // const leftDrawerOpen = ref(false);
+
+    return {};
+  },
+});
+</script>
