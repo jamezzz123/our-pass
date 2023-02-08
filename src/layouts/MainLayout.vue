e9fad586bc474249e91d0b3bff19e9414fb7334b
jamezzz123 <chinwokwuj@gmail.com>
2023-02-08 17:20:38 +0100

    refactor: implementing theme and layoutheader

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,25 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header>
+    <q-header class="bg-grey-1 text-primary">
       <q-toolbar>
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title class="text-weight-bolder text-h5 text-primary">
+          PayPay.
+        </q-toolbar-title>
+        <div>
+          <ul class="flex justify-between nav">
+            <li
+              class="q-mx-md text-h6 nav"
+              v-for="(list, index) in lists"
+              :key="index"
+            >
+              <q-icon
+                class="q-mx-sm text-grey-5"
+                size="sm"
+                :name="list.icon"
+              />{{ list.name }}
+            </li>
+          </ul>
+        </div>
       </q-toolbar>
     </q-header>
 
@@ -23,8 +39,45 @@ export default defineComponent({
 
   setup() {
     // const leftDrawerOpen = ref(false);
+    let lists = [
+      {
+        name: 'Dashboard',
+        icon: 'fas fa-th-large',
+      },
+      {
+        name: 'Invoice',
+        icon: 'fas fa-receipt',
+      },
+      {
+        name: 'Wallet',
+        icon: 'fas fa-wallet',
+      },
+      {
+        name: 'Activity',
+        icon: 'fas fa-chart-bar',
+      },
+      {
+        name: 'Help',
+        icon: 'fas fa-phone-alt',
+      },
+    ];
 
-    return {};
+    return { lists };
   },
 });
 </script>
+
+<style scoped>
+.nav {
+  list-style: none;
+}
+
+.container {
+  width: 100%;
+  max-width: 1240px;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+}
+</style>
