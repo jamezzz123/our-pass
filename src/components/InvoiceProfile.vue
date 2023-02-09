3805c668e62a2ec7ca01486cf4a806ddb655f550
jamezzz123 <chinwokwuj@gmail.com>
2023-02-09 08:47:14 +0100

    refactor: added Invoice Profile Component

---
+++ src/components/InvoiceProfile.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="row wrap text-white justify-between items-center bg-primary q-my-md q-py-md q-px-md card-radius"
+  >
+    <div class="col text-body1 text-capitalize">
+      <p class="q-mb-sm text-weight-bold">Invoice Number</p>
+      <p class="q-mb-sm">INV-2020-010</p>
+      <p class="q-mb-sm">Issued Date: 11 Jan 2022</p>
+      <p class="q-mb-sm">Due Date:18 Jan 2022</p>
+    </div>
+    <div class="col text-right text-body1 text-capitalize">
+      <p class="q-mb-sm text-weight-bold">Billed to</p>
+      <p class="q-mb-sm">Zaky Grizzly</p>
+      <p class="q-mb-sm">Moonlight Agency LTD</p>
+      <p class="q-mb-sm">New York,USA</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- src/pages/InvoicePage.vue
+++ src/pages/InvoicePage.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    <div class="q-my-lg text-h6">
+    <div class="q-my-md text-h6">
       <span class="text-primary">Invoice</span> / edit invoice
     </div>
 
-    div
+    <div class="card-radius bg-white">
+      <InvoiceProfile />
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import InvoiceProfile from 'src/components/InvoiceProfile.vue';
+</script>
 
-<style scoped></style>
+<style scoped>
+.card-radius {
+  border-radius: 15px;
+}
+</style>
