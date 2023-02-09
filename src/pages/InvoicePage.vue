99cf8fa51321796d02c3d3ea4241a280864236e9
jamezzz123 <chinwokwuj@gmail.com>
2023-02-09 08:19:43 +0100

    refactor: implementing invoice page WIP

---
+++ src/pages/InvoicePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="q-my-lg text-h6">
+      <span class="text-primary">Invoice</span> / edit invoice
+    </div>
+
+    div
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
