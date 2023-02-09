611bf73ce67afbf9583de467ba9f6a596abd4186
jamezzz123 <chinwokwuj@gmail.com>
2023-02-09 17:31:01 +0100

    refactor: implemented  a base Input compontent

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <input :type="type" :value="modelValue" @input="updateInput" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: 'text',
+    },
+  },
+  methods: {
+    updateInput(event: any) {
+      this.$emit('update:modelValue', event.target.value);
+    },
+  },
+});
+</script>
+
+<style scoped>
+input {
+  outline: none;
+  outline-style: none;
+  padding: 10px 20px;
+  width: 100%;
+  border-radius: 8px;
+  appearance: none;
+  border: 1px solid #9e9e9e;
+  background-color: #fafafa;
+  color: #4b48a3;
+  font-weight: 600;
+}
+</style>
