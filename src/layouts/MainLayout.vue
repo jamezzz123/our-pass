6d39aab16813b4731893622fad58b6116bc351dd
jamezzz123 <chinwokwuj@gmail.com>
2023-02-09 08:19:22 +0100

    refactor: add style  to mainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,30 +1,34 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header class="bg-grey-1 text-primary">
-      <q-toolbar>
-        <q-toolbar-title class="text-weight-bolder text-h5 text-primary">
-          PayPay.
-        </q-toolbar-title>
-        <div>
-          <ul class="flex justify-between nav">
-            <li
-              class="q-mx-md text-h6 nav"
-              v-for="(list, index) in lists"
-              :key="index"
-            >
-              <q-icon
-                class="q-mx-sm text-grey-5"
-                size="sm"
-                :name="list.icon"
-              />{{ list.name }}
-            </li>
-          </ul>
-        </div>
-      </q-toolbar>
+    <q-header class="bg-white text-primary">
+      <div class="container">
+        <q-toolbar>
+          <q-toolbar-title class="text-weight-bolder text-h5 text-primary">
+            PayPay.
+          </q-toolbar-title>
+          <div>
+            <ul class="flex justify-between nav">
+              <li
+                class="q-mx-md text-h6 nav"
+                v-for="(list, index) in lists"
+                :key="index"
+              >
+                <q-icon
+                  class="q-mx-sm text-grey-5"
+                  size="sm"
+                  :name="list.icon"
+                />{{ list.name }}
+              </li>
+            </ul>
+          </div>
+        </q-toolbar>
+      </div>
     </q-header>
 
-    <q-page-container>
-      <router-view />
+    <q-page-container class="bg-grey-1">
+      <div class="container" style="min-height: 100vh">
+        <router-view />
+      </div>
     </q-page-container>
   </q-layout>
 </template>
